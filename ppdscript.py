572229194ce0ed08e6a1c5950de41f8ea1dd2308
Jason Whittle <jason.j.whittle@gmail.com>
2020-08-08 14:42:50 +1000

    Update ppdscript.py

--- ppdscript.py
+++ ppdscript.py
@@ -1,1 +1,19 @@
+# 1.0 IMPORTING LIBRARIES -------------------------------------------------------------------
+#region
+import pandas as pd
+import numpy as np
+import os
+import re
+import glob
+import csv
+import shutil
+print('Libs Imported')
+#endregion
 
+#region
+filename = r'C:\E.xlsx'
+Sheet = 'Edval Events'
+
+df_data = pd.read_excel(filename,
+                        sheet_name = Sheet)
+#endregion
